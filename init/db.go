2d2073e83ca235adaf86533bf79fb5a89b349e3b
Matheus <matheudbaf0123@gmail.com>
2025-02-08 01:43:03 -0300

    fix database conection to use data from .env

--- init/db.go
+++ init/db.go
@@ -9,11 +9,9 @@ var DB *gorm.DB
 
 func InitDB() {
 	var err error
-	// Define the database configuration
-	dsn := "host=127.0.0.1 user=postgres password=doctorwho3210 dbname=golang_api_study port=5432"
 
 	// Initialize the database connection
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(GlobaConfig.DatabaseURL), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to the database")
 	}
